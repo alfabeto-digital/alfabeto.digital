29cbcfeafcd5ff5502950aa815d8ff0f7b3543f1
alfabeto.digital <services@alfabeto.digital>
2026-05-15 01:58:30 -0500

    authelia forwarding

--- nixos/modules/nixos/security/authelia.nix
+++ nixos/modules/nixos/security/authelia.nix
@@ -65,7 +65,10 @@
         default_2fa_method = "totp";
         log.level          = "info";
 
-        server.address = "tcp://127.0.0.1:${toString cfg.authelia_port}";
+        server = {
+          address         = "tcp://127.0.0.1:${toString cfg.authelia_port}";
+          trusted_proxies = [ "127.0.0.1/8" "::1/128" ];
+        };
 
         storage.postgres = {
           address  = "unix:///run/postgresql";
