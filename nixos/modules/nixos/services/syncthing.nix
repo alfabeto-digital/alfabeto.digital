e62a538e591ad3093540d0336e2c0d5b9c095d61
alfabeto.digital <services@alfabeto.digital>
2026-05-21 21:45:44 -0500

    syncthing data/config Dir

--- nixos/modules/nixos/services/syncthing.nix
+++ nixos/modules/nixos/services/syncthing.nix
@@ -6,12 +6,14 @@
     ];
 
     services.syncthing = {
-      enable     = true;
-      user       = cfg.syncthing_username;
-      group      = "storage";
-      dataDir    = "${storagePath}/helios";
-      configDir  = "${exchangePath}/syncthing";
-      guiAddress = "127.0.0.1:${toString cfg.syncthing_port}";
+      enable          = true;
+      user            = cfg.syncthing_username;
+      group           = "storage";
+      dataDir         = "${storagePath}/helios";
+      configDir       = "${exchangePath}/syncthing";
+      guiAddress      = "127.0.0.1:${toString cfg.syncthing_port}";
+      overrideDevices = false;
+      overrideFolders = false;
       settings.gui.insecureSkipHostcheck = true;
     };
   };
